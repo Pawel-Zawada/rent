b3da1988b5dd4a3d5956070a45d2d3ade6263964
Pawel Zawada <zawadapawelmail@gmail.com>
2018-03-05 12:47:52 +0100

    New columnds

--- src/Entity/Klant.php
+++ src/Entity/Klant.php
@@ -11,23 +11,48 @@ class Klant
 {
     /**
      * @ORM\Id
-     * @ORM\Column(type="string")
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer", length=11)
      */
     private $klantcode;
 
+    /**
+     * @ORM\Column(type="string", length=11)
+     */
     private $voorletters;
 
+    /**
+     * @ORM\Column(type="string", length=11)
+     */
     private $tussenvoegsels;
 
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
     private $achternaam;
 
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
     private $adres;
 
+    /**
+     * @ORM\Column(type="string", length=7)
+     */
     private $postcode;
 
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
     private $woonplaats;
 
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
     private $gebruikersnaam;
 
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
     private $wachtwoord;
 }
