af9aef5cb67b3a22b31e2a42c1c85cbdf8b9df0a
Pawel Zawada <zawadapawelmail@gmail.com>
2018-03-06 11:45:53 +0100

    some stuff

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -2,16 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Factuur;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class InvoiceController extends Controller
 {
     /**
      * @Route("/invoice")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('home/home.html.twig');
+        $user = $this->getUser();
+        $factuur = new Factuur();
+        $factuur->setFactuurdatum(getdate());
+
+        $form = $this->createFormBuilder($factuur)
+            ->add('kenteken', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create factuur'))
+            ->getForm();
+        return $this->render('invoice/invoice.html.twig', ['user' => $user, 'form' => $form->createView()]);
     }
 }
--- src/Entity/Factuur.php
+++ src/Entity/Factuur.php
@@ -17,6 +17,75 @@ class Factuur
     private $factuurnummer;
 
     /**
+     * @ORM\Column(type="string")
+     */
+    private $kenteken;
+
+    /**
+     * @return mixed
+     */
+    public function getKenteken()
+    {
+        return $this->kenteken;
+    }
+
+    /**
+     * @param mixed $kenteken
+     */
+    public function setKenteken($kenteken): void
+    {
+        $this->kenteken = $kenteken;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFactuurnummer()
+    {
+        return $this->factuurnummer;
+    }
+
+    /**
+     * @param mixed $factuurnummer
+     */
+    public function setFactuurnummer($factuurnummer): void
+    {
+        $this->factuurnummer = $factuurnummer;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFactuurdatum()
+    {
+        return $this->factuurdatum;
+    }
+
+    /**
+     * @param mixed $factuurdatum
+     */
+    public function setFactuurdatum($factuurdatum): void
+    {
+        $this->factuurdatum = $factuurdatum;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @param mixed $user_id
+     */
+    public function setUserId($user_id): void
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
      * @ORM\Column(type="date")
      */
     private $factuurdatum;
